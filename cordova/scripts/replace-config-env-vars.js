9fa57fcfe1fb130b6c3085ea9ea3558d01c61775
Johannes Eickhold <jeick@eclipsesource.com>
2016-12-19 15:33:49 +0100

    Use the BUILD_NUMBER from tabris-connect for Android and iOS artifacts
    This change should demonstrate the tabris-connect feature to supply the
    current BUILD_NUMBER to the build of an app where it can be used in a
    Cordova hook.

---
+++ cordova/scripts/replace-config-env-vars.js
@@ -0,0 +1,13 @@
+module.exports = function(context) {
+
+  const fs = context.requireCordovaModule('fs');
+  const cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util');
+
+  let projectRoot = cordova_util.isCordova();
+  let configXML = cordova_util.projectConfig(projectRoot);
+  let data = fs.readFileSync(configXML, 'utf8');
+  for (var envVar in process.env) {
+    data = data.replace('$' + envVar, process.env[envVar]);
+  }
+  fs.writeFileSync(configXML, data, 'utf8');
+};
