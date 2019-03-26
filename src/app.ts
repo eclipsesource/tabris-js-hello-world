e2df44602d1dcd57f04e868849087d03c8c3d703
Johannes Eickhold <jeick@eclipsesource.com>
2019-03-26 11:40:53 +0100

    Update to tabris release 3.0.0-beta2

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
 // This is a simple Tabris.js app. Feel free to modify as you please.
 
-import {Button, TextView, app, ui} from 'tabris';
+import {Button, TextView} from 'tabris';
 import * as tabris from 'tabris';
 
 // Create a push button and add it to the content view
 let button = new Button({
   centerX: 0, centerY: 0,
   text: 'Tap here'
-}).appendTo(ui.contentView);
+}).appendTo(tabris.contentView);
 
 // Create a text view and add it too
 let textView = new TextView({
   centerX: 0, bottom: [button, 20],
   font: '24px'
-}).appendTo(ui.contentView);
+}).appendTo(tabris.contentView);
 
 let tabrisVersion = (tabris as any).version;
 // Change the text when the button is pressed
