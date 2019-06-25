557d3d1b19dbef0313ae8d3d395569814460a13e
Johannes Eickhold <jeick@eclipsesource.com>
2019-06-25 13:15:08 +0000

    Prepare project to be used in https://gitpod.io

---
+++ .gitpod-devServer.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# This script is used when you open this git repository
+# in https://gitpod.io#https://github.com/eclipsesource/tabris-js-hello-world/tree/3.0.0
+
+export URL=$(gp url 8080 | cut -d \/ -f 1-3)
+export PATH=$PATH:$(pwd)/node_modules/.bin
+
+qrcode ${URL}
+echo Public URL of dev server is: ${URL}
+
+$(sleep 5 && gp preview $(gp url 8080)package.json)
+
+tabris serve -l
