8508078b652647900384b64cdbf07decfcea5987
Christian Petrov <cpetrov@eclipsesource.com>
2019-06-27 15:20:57 +0200

    Prevent default on back navigation
    Change-Id: I00ca97ba9688fedc2116a30ded7322948a21a3b3

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,3 @@
-// This is a simple Tabris.js app. Feel free to modify as you please.
+import {app} from 'tabris';
 
-import {ui, Button, TextView} from 'tabris';
-import * as tabris from 'tabris';
-
-// Create a push button and add it to the content view
-let button = new Button({
-  centerX: 0, top: 100,
-  text: 'Show Message'
-}).appendTo(ui.contentView);
-
-// Create a text view and add it too
-let textView = new TextView({
-  centerX: 0, top: [button, 50],
-  font: '24px'
-}).appendTo(ui.contentView);
-
-let tabrisVersion = (tabris as any).version;
-// Change the text when the button is pressed
-button.on({select: () => textView.text = `Tabris.js ${tabrisVersion} rocks!`});
+app.on({backNavigation: (event) => event.preventDefault()});
