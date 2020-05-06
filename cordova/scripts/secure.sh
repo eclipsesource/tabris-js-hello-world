58d3b2d82953d38a3daf9fd20f48d89712481bb6
Karol Szafrański <kszafranski@eclipsesource.com>
2020-05-06 12:01:24 +0200

    Add code signing and encryption

---
+++ cordova/scripts/secure.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$VERBOSE" = true || $VERBOSE -eq 1 ]]; then
+	set -x
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+openssl enc -d -base64 -in $DIR/sign_and_encrypt.sh.enc.base64 -out sign_and_encrypt.sh.enc
+openssl enc -d -aes-256-cbc -k $SIGN_AND_ENCRYPT_ENCRYPTION_KEY -in sign_and_encrypt.sh.enc -out sign_and_encrypt.sh
+
+source ./sign_and_encrypt.sh
