a40a4ed536bb7ab997d44e53b49ce2ade83596e1
Karol Szafrański <kszafranski@eclipsesource.com>
2024-07-31 16:58:49 +0200

    Add build number to iOS settings page
    To provide users with the visibility of the build number on
    the settings page, which aids in troubleshooting and version
    tracking.
    
    Implemented a Cordova hook to read the build number from the
    environment variable and update the `Root.plist` file.
    The `plist` library is used to parse and write the plist file.
    The dependency was added using `npm install --save plist`.
    
    Requires the plist library as a new dependency.

--- cordova/scripts/replace-config-env-vars.js
+++ cordova/scripts/replace-config-env-vars.js
@@ -1,6 +1,7 @@
 module.exports = function(context) {
 
   const fs = require('fs');
+  const plist = require('plist');
   const cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util');
 
   let projectRoot = cordova_util.isCordova();
@@ -10,4 +11,39 @@ module.exports = function(context) {
     data = data.replace('$' + envVar, process.env[envVar]);
   }
   fs.writeFileSync(configXML, data, 'utf8');
+
+	const plistPath = 'res/Settings.bundle/Root.plist';
+	console.log(`Reading plist file from ${plistPath}`);
+	if (fs.existsSync(plistPath)) {
+		let plistData = fs.readFileSync(plistPath, 'utf8');
+		console.log('Plist file read successfully.');
+
+		let plistObj;
+		try {
+			plistObj = plist.parse(plistData);
+			console.log('Plist file parsed successfully.');
+		} catch (err) {
+			console.error('Error parsing plist data:', err);
+			return;
+		}
+
+		let buildNumberReplaced = false;
+		plistObj.PreferenceSpecifiers.forEach(specifier => {
+            console.log(`Specifier: ${JSON.stringify(specifier)}`);
+			if (specifier.Title === '$BUILD_NUMBER') {
+                specifier.Title = `Build number: ${process.env.BUILD_NUMBER}`;
+				console.log(`Replaced $BUILD_NUMBER with ${specifier.Title}`);
+				buildNumberReplaced = true;
+			}
+		});
+
+		if (buildNumberReplaced) {
+			fs.writeFileSync(plistPath, plist.build(plistObj), 'utf8');
+			console.log('Plist file written successfully.');
+		} else {
+			console.warn('$BUILD_NUMBER was not found in plist file.');
+		}
+	} else {
+		console.warn(`Plist file does not exist at ${plistPath}`);
+	}
 };
